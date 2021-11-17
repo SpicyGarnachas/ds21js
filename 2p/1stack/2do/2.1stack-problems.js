2ee1f5210e60bf506c9c6c07f77609373eb90b85
Diego Martínez <56555056+SpicyGarnachas@users.noreply.github.com>
2021-11-17 12:45:07 -0600

    Add files via upload

--- 2p/1stack/2do/2.1stack-problems.js
+++ 2p/1stack/2do/2.1stack-problems.js
@@ -10,13 +10,46 @@
  *
  */
 
- let arrayStack = [];
- let genNumbers = () => {
-   for (let i = 0; i < 10000; i++){
-     arrayStack.push(Math.floor(Math.random() * 10000));
-   }
- }
- console.time('time');
- genNumbers();
- console.log(arrayStack);
- console.timeEnd('time');
+// Stacks implemented with an array
+let arrayStack = [];
+let genNumbers = () => {
+  for (let i = 0; i < 10000; i++){
+    arrayStack.push(Math.floor(Math.random() * 10000));
+  }
+}
+console.time('time');
+genNumbers();
+console.log(arrayStack);
+console.timeEnd('time');
+
+//Stacks implemented with a linked list
+let linkedListStack = {
+  head: null,
+  push: function(value){
+    let node = {
+      value: value,
+      next: null
+    };
+    if(this.head === null){
+      this.head = node;
+    }else{
+      let current = this.head;
+      while(current.next){
+        current = current.next;
+      }
+      current.next = node;
+    }
+  }
+}
+let genNumbers2 = () => {
+  for (let i = 0; i < 10000; i++){
+    linkedListStack.push(Math.floor(Math.random() * 10000));
+  }
+}
+console.time('time2');
+genNumbers2();
+console.log(linkedListStack);
+console.timeEnd('time2');
+
+
+
--- 2p/2queue/2do/2.2queue-problems.js
+++ 2p/2queue/2do/2.2queue-problems.js
@@ -42,3 +42,5 @@ queue.enqueue(1);
 queue.enqueue(2);
 queue.enqueue(3);
 queue.enqueue(4);
+
+console.log(queue.element);
